10a46738e86ff26dfbc5eaa7c8cd569c4269ae4f
Rohan Udupa <86579136+lab596@users.noreply.github.com>
2022-07-17 12:58:56 -0700

    Update rstories.py

--- rstories.py
+++ rstories.py
@@ -4,14 +4,15 @@ from random import randint
 import requests
 import shutil
 import os
+import time
 
 
 
 
-reddit = praw.Reddit(client_id = "*",
-                      client_secret= "*",
+reddit = praw.Reddit(client_id = "",
+                      client_secret= "",
                       username="DaT1dUdE05",
-                      password= "*",
+                      password= "",
                       user_agent="pythonmeme",
                       check_for_async=False
                     )
@@ -52,11 +53,14 @@ for i in range(1):
 url = random_sub.url
 submission = reddit.submission(url = url)
 
+comments = []
+
 num = 5
 submission.comments.replace_more(limit=None)
 for top_level_comment in submission.comments:
     if num>0:
         print(top_level_comment.body)
+        comments.append(top_level_comment.body)
         print()
     num-=1
 
@@ -70,7 +74,7 @@ from selenium.webdriver.chrome.service import Service
 
 
 
-    
+#Finding and taking screenshot of question   
 path=Service("C:\chromedriver_win32 (1)\chromedriver.exe")
 driver = webdriver.Chrome(service=path)
 
@@ -90,6 +94,15 @@ driver.execute_script("window.open('about:blank', 'secondtab');")
 driver.switch_to.window("secondtab")
 driver.get('https://translate.google.com/')
 
+#Finding Text Area to Type in
+#for i in comments:
+driver.find_element(By.XPATH,'//*[@id="yDmH0d"]/c-wiz/div/div[2]/c-wiz/div[2]/c-wiz/div[1]/div[2]/div[3]/c-wiz[1]/span/span/div/textarea').send_keys(random_sub.title)
+driver.find_element(By.XPATH,'//*[@id="i8"]/span[3]').click()
+time.sleep(5)
+#driver.find_element(By.XPATH,'//*[@id="ow87"]/div/span/button/div[3]').click()
+#time.sleep(2)
+    
+
 
 from pywinauto.application import Application
 
@@ -99,8 +112,6 @@ app = Application(backend='uia').start('C:\Program Files\Audacity\Audacity.exe')
 
 rec = app.Audacity.child_window(title="Record", auto_id="11005", control_type="Button").wrapper_object()
 rec.click()
-
-
 
 
 
